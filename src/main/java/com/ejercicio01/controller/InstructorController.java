d202f5afc4f50ae8ede2bf78cfb9374f89f163ae
DreamsBR <marcosbr301@gmail.com>
2021-10-07 15:06:32 -0500

    Commit2

--- src/main/java/com/ejercicio01/controller/InstructorController.java
+++ src/main/java/com/ejercicio01/controller/InstructorController.java
@@ -23,7 +23,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/instructor")
-@Api(value = "/instructor")
 public class InstructorController {
     @Autowired
     private InstructorService service;
